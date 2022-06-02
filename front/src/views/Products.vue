ddea4c5f88c6eebc1aa44fbdfaf77dddfb8d2255
juan rincon <juanhit471523@gmail.com>
2022-06-02 15:20:54 -0500

    listado productos

--- front/src/views/Products.vue
+++ front/src/views/Products.vue
@@ -1,10 +1,12 @@
 <template>
   <button @click="show">Listar productos</button>
+  <ul class="wrapper-list">
   <li v-for="(producto, index) in productos.content" :key="index">
-    <div>
+    <div class="wrapper-Barber">
       <p>{{ producto.decription }}</p>
     </div>
   </li>
+  </ul>
 </template>
 <script>
 import { onMounted } from 'vue'
@@ -31,4 +33,18 @@ export default {
   }
 }
 </script>
-<style></style>
+<style scoped lang="css">
+.wrapper-list {
+  list-style: none;
+  padding: 2rem;
+  background-color: #cd9f6e;
+}
+.wrapper-Barber {
+  width: 30%;
+  border-radius: 0.8rem;
+  background-color: #9b6b37;
+  padding: 0.3rem;
+  margin-bottom: 1rem;
+  cursor: pointer;
+}
+</style>
